b079ebbc59a33b6567ffc004fa7901b9dd2e2798
nelsonaq <chibuds217@gmail.com>
2022-03-18 10:02:47 +0800

    made some changes in design and started hex color functionality

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 "use strict";
+const nav = document.querySelector(".nav");
+const randomBtn = document.querySelector(".btn");
+const colorName = document.querySelector("div > p");
+
+const randomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomColorHex = function () {
+  const hexCharacters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  let hexColor = "#";
+  for (let x = 0; x < 6; x++) {
+    hexColor += hexCharacters[randomNumber(0, hexCharacters.length - 1)];
+  }
+  console.log(hexColor);
+  colorName.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+};
+
+randomBtn.addEventListener("click", randomColorHex);
+
+nav.addEventListener("click", function (e) {
+  const colorType = e.target.closest("h1");
+  console.log(colorType);
+});
